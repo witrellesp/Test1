b1c32c02149e64ae76475362571e469233349496
witrellesp <william.trelles1@eduvaud.ch>
2024-02-20 12:01:56 +0100

    Ajout d'un commentaire dans script.cpp sur DEV-WTP

--- Code/Module1/script.cpp
+++ Code/Module1/script.cpp
@@ -1,7 +1,8 @@
 #Mon 1er programme
 void Main()
 	{
-		# Commentaire supplémentaire 
+		# Commentaire supplémentaire
+		# Ce commentaire vient de la branche DEV-WTP
 		Console.Writeline();
 		Console.Writeline("Hello World !!");
 		Console.Writeline();	
